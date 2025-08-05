14aeba9117a50d996e27aecfb1a4ef9786ec40fa
Godtasan552 <werawat7903@gmail.com>
2025-08-05 23:50:46 +0700

    edit drower remove 1 listtile

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -53,19 +53,7 @@ class _MyDrawerState extends State<MyDrawer> {
               Get.to(() => const SettingsPage());
             },
           ),
-          ListTile(
-            title: const Text('Print data (debug)'),
-            onTap: () async {
-              Navigator.pop(context);
-              final users = await StorageService.loadUsers();
-              final tasks = await StorageService.loadTasks();
-              final encoder = const JsonEncoder.withIndent('  ');
-              final usersJson = encoder.convert(users.map((e) => e.toJson()).toList());
-              final tasksJson = encoder.convert(tasks.map((e) => e.toJson()).toList());
-              debugPrint("🧑‍🦱 Users:\n$usersJson");
-              debugPrint("📋 Tasks:\n$tasksJson");
-            },
-          ),
+          
 
         ],
       ),
